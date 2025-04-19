409b9c5d2e23d30de1c331f21e87f86e0a47bf43
junojobin <junojamesv@gmail.com>
2025-04-19 16:42:18 -0600

    Initial Commit

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,31 @@
+import UIKit
+
+// VARIABLE
+var str = "Hello, playground"
+str = "Bye"
+
+//STRING and INTEGERS
+var age = 30
+var population = 8_000_000
+
+//MULTI LINE STRING
+var str1 = """
+This goes
+over multiple
+lines
+"""
+
+//DOUBLES and BOOLEANS
+var pi = 3.141
+var awsome = true
+
+//STRING INTERPOLATION
+var score = 85
+var scoreVal = "Your score was \(score)"
+
+//CONSTANTS
+let taylor = "Swift"
+
+//TYPE ANNOTATIONS - Type Inference
+let string = "Hey How are you?"
+let year: Int = 1990
